9bfe0d8f9d047bf328699ecc420d15dc1e263ebc
Manne Busk <mannebusk@gmail.com>
2015-05-20 22:26:37 +0200

    Added .gvimrc to install script

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 ln -s $DIR/.vim ~/.vim
 ln -s $DIR/.vimrc ~/.vimrc
+ln -s $DIR/.gvimrc ~/.gvimrc
 echo "Symbolic links for .vim and .vimrc was made in your home directory"
 echo "Enjoy!"
